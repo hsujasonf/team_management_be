79903fe0bf167c49b9286ab5e329a298f6402ce2
Jason Hsu <hsujasonf@gmail.com>
2024-05-01 22:29:44 -0700

    team_management_be

---
+++ team_management/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('regular', 'Regular'),
+    )
+    role = models.CharField(max_length=7, choices=ROLE_CHOICES, default='regular')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
